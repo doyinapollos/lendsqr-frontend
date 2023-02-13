2659495e405cd3c3debf1b398a60f30b01e78169
Chukwuebuka <Chukwuebuka.tech@gmail.com>
2023-02-13 23:07:21 +0100

    Chukwuebuka: Fonts Setup

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,10 +1,25 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-    content: [
-        "./src/**/*.{js,jsx,ts,tsx}",
-    ],
-    theme: {
-        extend: {},
-    },
-    plugins: [],
-}
+	content: ["./src/**/*.{js,jsx,ts,tsx}"],
+	darkMode: "class", // or 'media' or 'class'
+	theme: {
+		extend: {
+			fontFamily: {
+				avenirNextBold: "'AvenirNextBold', serif",
+				avenirNextLight: "'AvenirNextLight', serif",
+				avenirNextRegular: "'AvenirNextRegular', serif",
+			},
+			spacing: {
+				is01px: "1px",
+				529: "529px",
+			},
+			colors: {
+				"lendsqr-red": "#ED2025",
+			},
+		},
+	},
+	variants: {
+		extend: {},
+	},
+	plugins: [],
+};
